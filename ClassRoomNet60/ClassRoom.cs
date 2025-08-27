ed57603dbd13b2ca10e98bbfbc775b2107eadea9
Infinity-MaxX <katerina@oestergaard.com>
2025-08-27 12:22:12 +0200

    Created ClassRoom class.

---
+++ ClassRoomNet60/ClassRoom.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class ClassRoom
+    {
+        #region Instances
+        #endregion
+
+        #region Properties
+        string ClassName { get; set; }
+        List<Student> StudentList { get; set; }
+        DateTime SemesterStart { get; set; }
+        #endregion
+
+        #region Constructor
+        public ClassRoom() { }
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return $"";
+        }
+        #endregion
+    }
+}
