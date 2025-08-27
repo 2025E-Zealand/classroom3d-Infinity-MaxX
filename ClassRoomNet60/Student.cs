83e2b2be1331199fc06c2e83922cdc3a2cbc4759
Infinity-MaxX <katerina@oestergaard.com>
2025-08-27 12:17:02 +0200

    Made Student class.

---
+++ ClassRoomNet60/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class Student
+    {
+        #region Instances
+
+        #endregion
+
+        #region Properties
+        public string Name { get; private set; }
+        public int BirthMonth { get; private set; }
+        public int Birthday { get; private set; } 
+        #endregion
+
+        #region Constructor
+        public Student(string name, int birthMonth, int birthday)
+        {
+            Name = name;
+            BirthMonth = birthMonth;
+            Birthday = birthday;
+        }
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return $"Name: {Name}\nBirthday: {Birthday}/{BirthMonth}";
+        }
+        #endregion
+    }
+}
